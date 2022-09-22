a03faefe62d030ccedbe4b44531eac9198cd84e4
Kingkpoma <kworenwu@gmail.com>
2022-09-22 14:39:11 -0700

    task 8

--- 100-rot13.c
+++ 100-rot13.c
@@ -5,21 +5,22 @@
  * Return: address of s
  */
  char * rot13(char *s)
-{
-int i, j;
-char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+ {
+ int i, j;
+ char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-for (i = 0; *(s + i); i++)
-{
-for (j = 0; j < 52; j++)
-{
-if (a[j] == *(s + i)
-{
-*(s + i) = b[j];
+ for (i = 0; *(s + i); i++)
+ {
+ for (j = 0; j < 52; j++)
+ {
+ if (a[j] == *(s + i)
+ {
+ *(s + i) = b[j];
  break;					
-}
-}
-return (s);
-}
+ }
+ }
+ }
+ return (s);
+ }
 
