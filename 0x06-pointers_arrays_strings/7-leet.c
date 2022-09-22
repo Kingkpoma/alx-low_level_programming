ebd4179a675d0b7d3c0156a054c1d40910862cee
Kingkpoma <kworenwu@gmail.com>
2022-09-22 09:12:04 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,23 +2,21 @@
 
 /**
 * leet - encodes a string into 1337
-* @s: string to encod
+* @s: string to encode
 *
-* Return: address of s
+*  Return: address of s
 */
-
 char *leet(char *s)
 {
-char *leet(char *s)
-char *leet(char *s)
+int i, j;
+char a[] = "aAeEoOtTlL";
 char b[] = "4433007711";
 for (i = 0; *(s + i); i++)
-{
-for (j = 0; j <= 9; j++){
+for (j = 0; j <= 9; j++)
 {
 if (a[j] == *(s + i))
 *(s + i) = b[j];
 }
 }
-*(s + i) = b[j];
+return (s);
 }
